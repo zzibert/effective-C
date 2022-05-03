db70f94ed4eb0130820057f48f1278dc5db7a209
Žan Žibert <zzibert@plume.com>
2022-05-03 13:56:43 +0200

    Declaring variables

---
+++ variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void swap(int, int); // defined in Listing 2-2
+
+int main(void) {
+    int a = 27;
+    int b = 17;
+
+    swap(a, b);
+    printf("main: a = %d, b = %d\n", a, b);
+    return 0;
+}
