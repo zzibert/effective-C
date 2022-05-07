1b3c3b28e45e85f48b0752936cc646c26faacb0a
Žan Žibert <zzibert@plume.com>
2022-05-07 12:21:54 +0200

    Break Statement 2

--- break.c
+++ break.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main(void) {
-    char c;
-    for(;;) {
-        puts("Press any key, Q to quit: ");
-        c = toupper(getchar());
-        if (c == 'Q') break;
+size_t find_element(size_t len, int arr[len], int key) {
+    size_t pos = (size_t)-1;
+
+    // traverse arr and search for key
+    for (size_t i = 0; i < len; ++i) {
+        if (arr[i] == key) {
+            pos = i;
+            break;
+        }
     }
-} // loop exists only when the uppercase of lowercase 'Q' is pressed
+    return pos;
+}
+
+int main(void) {
+    int array[6] = {'a', 'B', 'c', 'D', 'e', 'F'};
+    char element = 'g';
+
+    printf("Element %c is on position %zu", element, find_element(6, array, element));
+}
