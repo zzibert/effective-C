fe304518c006c8d329ca83f34550f18dd274161a
Žan Žibert <zzibert@plume.com>
2022-05-07 10:00:57 +0200

    Type cast

---
+++ cast-operators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    double x = 1.2;
+    int sum = (int)x + 1; // Explicit conversion from double to int
+
+    printf("sum %d", sum);
+
+
+}
