a9151bb4021795bbe5df78ef5dcdab053aebb628
Žan Žibert <zzibert@plume.com>
2022-05-08 19:05:33 +0200

    Size and length of strings

---
+++ size_and_length.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char str[100] = "Here comes the Sun";
+    size_t str_size = sizeof(str); // str_size is 100
+
+    printf("Size of string is %zu\n", str_size);
+
+    size_t str_len = strlen(str);
+
+    printf("Length of string is %zu\n", str_len);
+}
