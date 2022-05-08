070e8ec456ca47b72921a12fc0c67bf76670d202
Žan Žibert <zzibert@plume.com>
2022-05-08 19:22:50 +0200

    Copying a string

--- size_and_length.c
+++ size_and_length.c
@@ -2,15 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+size_t strlen(const char * str) {
+    const char *s;
+    for (s = str; *s; ++s) {}
+    return s - str;
+}
+
 int main(void) {
     char str[100] = "Here comes the Sun";
-    size_t str_size = sizeof(str); // str_size is 100
+    size_t str_size = strlen(str) + 1; // str_size is 100
+
+    char *dest = (char *)malloc(str_size);
+
+    if (dest) {
+        strcpy(dest, str);
+    } else {
+        // handle error
+    }
 
     printf("Size of string is %zu\n", str_size);
 
     size_t str_len = strlen(str);
 
     printf("Length of string is %zu\n", str_len);
+
 
     // dynamically allocationg storage for narrow string functions
 
