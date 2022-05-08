805acdb1b363db88e4aee41c1711367af9d7183f
Žan Žibert <zzibert@plume.com>
2022-05-08 10:21:15 +0200

    Using a VLA as a function parameter

--- flexible-array-members.c
+++ flexible-array-members.c
@@ -19,3 +19,14 @@ void *func(size_t array_size) {
         p->data[i] = 17;
     }
 }
+
+int matrix_sum(size_t rows, size_t cols, int m[rows][cols]) {
+    int total = 0;
+
+    for (size_t r = 0; r < rows; r++) {
+        for (size_t c = 0; c < cols; c++) {
+            total += m[r][c];
+        }
+    }
+    return total;
+}
